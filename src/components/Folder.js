3b132780d3b48753e1c36277be53df7d56438f6e
Aadhar Aggarwal <aadharoct@gmail.com>
2023-07-19 22:28:43 +0530

    first commit

---
+++ src/components/Folder.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+function Folder({ handleInsertNode, explorer }) {
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+
+  const handleNewFolder = (e, isFolder) => {
+    e.stopPropagation();
+    setExpand(true);
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+
+  const onAddFolder = (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      handleInsertNode(explorer.id, e.target.value, showInput.isFolder);
+      setShowInput({ ...showInput, visible: false });
+    }
+  };
+
+  console.log(explorer);
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div className="folder" onClick={() => setExpand(!expand)}>
+          <span>📁 {explorer.name}</span>
+
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
+        </div>
+
+        <div
+          style={{ display: expand ? "block" : "none", paddingLeft: "25px" }}
+        >
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                autoFocus
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                onKeyDown={onAddFolder}
+                className="inputContainer__input"
+              />
+            </div>
+          )}
+
+          {explorer.items.map((exp) => {
+            // return <span>{exp.name}</span>;
+            return (
+              <Folder
+                explorer={exp}
+                key={exp.id}
+                handleInsertNode={handleInsertNode}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return <span className="file">📄 {explorer.name}</span>;
+  }
+}
+
+export default Folder;
